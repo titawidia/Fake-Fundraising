800ff3c6f8914217904319dd754ac05f5a14417a
titawidia <gitawidia166@gmail.com>
2023-04-05 08:58:39 +0700

    Initial commit

---
+++ src/main/java/com/itc/fakefundraising/FakeFundraisingApplication.java
@@ -0,0 +1,13 @@
+package com.itc.fakefundraising;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FakeFundraisingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FakeFundraisingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itc/fakefundraising/FakeFundraisingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itc.fakefundraising;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeFundraisingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
